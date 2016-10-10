981a7a4068a5168ee30c90a2e3582203c147a801
anatoly <pavlovanatoly2012@gmail.com>
2016-10-10 14:09:42 -0700

    Added sys args to main module and saving of cleanned data

--- simple_model/simple_main.py
+++ simple_model/simple_main.py
@@ -4,6 +4,7 @@ loads row data and outputs, if it is directed so, post-processed data and
 it also saves the model in .pkl file.'''
 
 import pandas as pd
+import sys
 from datetime import timedelta
 
 # Custom Modules:
@@ -48,29 +49,47 @@ def show_days_details(df, day_index=0):
 
 
 if __name__ == '__main__':
-    path_data =\
-    '../data/Power-Networks-LCL-June2015(withAcornGps).csv_Pieces/Power-Networks-LCL-June2015(withAcornGps)v2_1.csv'
-    path_data2 =\
-    '../data/Power-Networks-LCL-June2015(withAcornGps).csv_Pieces/Power-Networks-LCL-June2015(withAcornGps)v2_2.csv'
-    df = pd.read_csv(path_data)
-    households = show_households(df)
-    household = households[0]
+    action = sys.argv[1]
+    if action == 'data':
+        path_data =\
+        '../data/Power-Networks-LCL-June2015(withAcornGps).csv_Pieces/Power-Networks-LCL-June2015(withAcornGps)v2_1.csv'
+        path_data2 =\
+        '../data/Power-Networks-LCL-June2015(withAcornGps).csv_Pieces/Power-Networks-LCL-June2015(withAcornGps)v2_2.csv'
+        print
+        print '## Loading Data'
+        print
+        df = pd.read_csv(path_data)
+        households = show_households(df)
+        household = households[0]
 
-    ch = ChooseHousehold(household)
-    df = ch.transform(df)
-    #
-    csf = ConvertStrFloat('KWH/hh (per half hour) ')
-    df = csf.transform(df)
-    #
-    cd = CleanData(datetime_col='DateTime', yt_col='KWH/hh (per half hour) ')
-    df = cd.drop_duplicate_records(df)
-    df = cd.drop_missing_val(df)
-    df = cd.drop_null_val(df)
-    df = cd.drop_incomplete_days(df)
-    #
-    ets = ExtractTimeSeries(datetime_col='DateTime', yt_col='KWH/hh (per half hour) ')
-    df = ets.transform(df)
-    #
-    show_days_details(df)
-    print
-    print 'Above results are for the household_id: {}'.format(household)
+        print
+        print '## Data Preprocessing'
+        print
+        ch = ChooseHousehold(household)
+        df = ch.transform(df)
+        #
+        csf = ConvertStrFloat('KWH/hh (per half hour) ')
+        df = csf.transform(df)
+        #
+        cd = CleanData(datetime_col='DateTime', yt_col='KWH/hh (per half hour) ')
+        df = cd.drop_duplicate_records(df)
+        df = cd.drop_missing_val(df)
+        df = cd.drop_null_val(df)
+        df = cd.drop_incomplete_days(df)
+        #
+        ets = ExtractTimeSeries(datetime_col='DateTime', yt_col='KWH/hh (per half hour) ')
+        df = ets.transform(df)
+        #
+        '''show_days_details(df)
+        print
+        print 'Above results are for the household_id: {}'.format(household)'''
+
+        print
+        print '## Saving Data'
+        print
+        df.to_csv('../clean_data/'+household+'.csv')
+
+    if action == 'model':
+        print
+        print '## Training Model'
+        print
