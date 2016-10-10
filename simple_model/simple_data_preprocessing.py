1db5a6d9841c2d4feba3b53d87a699b0806a00e4
anatoly <pavlovanatoly2012@gmail.com>
2016-10-09 19:16:23 -0700

    Added CleanData class and updated some methods

--- simple_model/simple_data_preprocessing.py
+++ simple_model/simple_data_preprocessing.py
@@ -2,12 +2,14 @@
 before using it to train a model. It is designed to pre-process data of London
 household smart meters readings of electric power consomption. The output of
 the pipeline will be the time series as pandas pd.DataFrame object, where date-time
-will be the index of the pd.DataFrame while corresponding measurements of the power
-set as values in a single column in the pd.DataFrame object.'''
+stamp will be the index of the pd.DataFrame while corresponding measurements
+of the power set as values in a single column in the pd.DataFrame object.'''
 
 import pandas as pd
 import numpy as np
 from sklearn.pipeline import Pipeline, make_pipeline
+from datetime import timedelta
+from collections import Counter
 
 pd.options.mode.chained_assignment = None
 
@@ -20,6 +22,37 @@ def show_households(df):
     print
     return df['LCLid'].unique()
 
+def find_all_indexes(lst, item):
+    return [i for i, x in enumerate(lst) if x==item]
+
+def show_days_details(df, day_index=0):
+    datetimes = df.index
+    days = []
+    for i, datetime in enumerate(datetimes):
+        if datetime.date() not in days:
+            days.append(datetime.date())
+
+    print 'Printing days where number of records is not 48.'
+    print
+    for i, day in enumerate(days):
+        next_day = day + timedelta(days=1)
+        df_day = df.query('index >= @day and index < @next_day')
+        if len(df_day) != 48:
+            print '{}, {}, {}'.format(i, day, len(df_day))
+    print
+    print 'Total number of days for this household: {}'.format(len(days))
+    print
+
+    # show a single day time-series
+    day = days[day_index]
+    next_day = day + timedelta(days=1)
+    df_day = df.query('index >= @day and index < @next_day')
+    print
+    print 'Time-series for {}. Todal number of records: {}'.format(day, len(df_day))
+    print
+    print df_day
+
+#========================================================================================
 class ChooseHousehold(object):
 
     def __init__(self, household_id=None):
@@ -40,6 +73,35 @@ class ConvertStrFloat(object):
         df[self.col_name] = pd.to_numeric(df[self.col_name], errors='coerce')
         return df
 
+class CleanData(object):
+
+    def __init__(self, datetime_col, yt_col):
+        self.datetime_col = datetime_col
+        self.yt_col = yt_col
+
+    def drop_duplicate_records(self, df):
+        df.drop_duplicates(subset=self.datetime_col, keep='first', inplace=True)
+
+    def drop_missing_val(self, df):
+        df.dropna(how='any', subset=[self.yt_col], thresh=1, inplace=True)
+
+    def drop_incomplete_days(self, df):
+        datetimes = pd.to_datetime(df[self.datetime_col])
+        #print datetimes
+        days = []
+        for i, datetime in enumerate(datetimes):
+            if datetime.date() not in days:
+                days.append(datetime.date())
+        #
+        days_to_drop = []
+        for i, day in enumerate(days):
+            next_day = day + timedelta(days=1)
+            df_day = df[(datetimes >= day) & (datetimes < next_day)]
+            if len(df_day) < 48:
+                days_to_drop.append(day)
+
+        print days_to_drop
+
 class ExtractTimeSeries(object):
 
     def __init__(self, datetime_col, yt_col):
@@ -53,20 +115,15 @@ class ExtractTimeSeries(object):
                                     index=df[self.datetime_col].values)
         return df_out
 
-class DropIncompleteDays(object):
-
-    def __init__(self):
-        pass
-
-    def transform(self, df):
-        pass
-
 
 if __name__ == '__main__':
-    path_data = '../data/Power-Networks-LCL-June2015(withAcornGps).csv_Pieces/Power-Networks-LCL-June2015(withAcornGps)v2_1.csv'
+    path_data =\
+    '../data/Power-Networks-LCL-June2015(withAcornGps).csv_Pieces/Power-Networks-LCL-June2015(withAcornGps)v2_1.csv'
+    path_data2 =\
+    '../data/Power-Networks-LCL-June2015(withAcornGps).csv_Pieces/Power-Networks-LCL-June2015(withAcornGps)v2_2.csv'
     df = pd.read_csv(path_data)
     households = show_households(df)
-    household = households[5]
+    household = households[0]
 
     ch = ChooseHousehold(household)
     df = ch.transform(df)
@@ -74,10 +131,18 @@ if __name__ == '__main__':
     csf = ConvertStrFloat('KWH/hh (per half hour) ')
     df = csf.transform(df)
     #
-    ets = ExtractTimeSeries('DateTime', 'KWH/hh (per half hour) ')
+    cd = CleanData(datetime_col='DateTime', yt_col='KWH/hh (per half hour) ')
+    cd.drop_duplicate_records(df)
+    cd.drop_missing_val(df)
+    cd.drop_incomplete_days(df)
+    #
+    ets = ExtractTimeSeries(datetime_col='DateTime', yt_col='KWH/hh (per half hour) ')
     df = ets.transform(df)
     #
-    print 'First and last 5 rows of time-series for the household_id: {}'.format(household)
+    show_days_details(df)
+    print
+    print 'Above results are for the household_id: {}'.format(household)
+    '''print 'First and last 5 rows of time-series for the household_id: {}'.format(household)
     print df.head()
     print
-    print df.tail()
+    print df.tail()'''
